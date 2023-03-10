a363b301722dfce37358ba2522d60d437c90ed47
Viacheslav Khromiak <slava.khromyak@gmail.com>
2023-03-10 08:32:36 +0200

    add logger

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import pyautogui
 import pywinauto
 import requests
 from time import sleep
+import logging
+
+
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.getLogger().addHandler(logging.StreamHandler())
 
 
 class Main:
     def __init__(self, port=21337):
+        logging.info('starting')
         self.url = f"http://127.0.0.1:{port}/positional-rectangles"
         self.position = (312, 40)
         self.state = 'disabled'
@@ -14,8 +20,9 @@ class Main:
     def get_game_state(self):
         try:
             self.state = requests.get(self.url).json()['GameState']
+            logging.info(f'game state: {self.state}')
         except requests.exceptions.ConnectionError:
-            print('game is not running')
+            logging.info('game is not running')
             self.state = 'off'
 
     def disable_emote(self):
@@ -28,16 +35,17 @@ class Main:
             if str(e) == 'There is no active desktop required for moving mouse cursor!':
                 return False
         pywinauto.mouse.move(coords=old_position)
+        logging.info('emote disabled')
         return True
 
     def valid_state(self):
         old_state = self.state
         self.get_game_state()
         if old_state in ['Menus', None] and self.state == 'InProgress':
-            self.rate = 10
+            self.rate = 20
             return True
         else:
-            self.rate = 1
+            self.rate = 3
 
     def run(self):
         while True:
